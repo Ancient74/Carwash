1ec59d0d140f36e37b9992950936af9802e8ff8c
Ancient <yura.ruban2011@gmail.com>
2020-06-29 18:51:02 +0300

    Remove token validation service since it is no longer needed

--- token-validation-service/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-HTTP_PORT = 3000
-HTTP_HOST = "127.0.0.1"
-
-KEYCLOAK_URL = "http://localhost:8080/auth/"
-USER_INFO_URL = f"{KEYCLOAK_URL}realms/carwash/protocol/openid-connect/userinfo"
